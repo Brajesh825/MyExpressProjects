458cfcf723a7edd14de2dc1c3d7ba06922e9137c
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-09 20:16:11 +0530

    Connected with atlas Database

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const express = require('express')
-const app = express()
-const tasks = require('./routes/tasks')
-const port = 3000
-
-// Middleware
-app.use(express.static('./public'))
-app.use(express.json())
-
 //Routes
 // app.get('/api/v1/tasks')             - get all the tasks
 // app.post('/api/v1/tasks')            - create a new task
@@ -14,8 +5,29 @@ app.use(express.json())
 // app.patch('/api/v1/tasks/:id')       - update task
 // app.delete('/api/v1/tasks/:id')      - delete task
 
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB =require('./db/connect')
+const port = 3000
+
+// Middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//Routes
 app.use('/api/v1/tasks',tasks)
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`)
-})
+//Initialiser
+const start = async () =>{
+    try {
+        await connectDB()
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
