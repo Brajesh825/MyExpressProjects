2cf389b478dcfaa1f43dde1ea764087f75c3d6c0
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-10 17:58:52 +0530

    Function to get one Task completed with error Handling

--- TaskManager/controllers/tasks.js
+++ TaskManager/controllers/tasks.js
@@ -14,12 +14,21 @@ const createTask = async (req,res)=>{
         const task = await Task.create(req.body)
         res.status(201).json(task)       
     } catch (error) {
-        res.status(500).json({msg:error.errors.name.message})
+        res.status(500).json({msg:error})
     } 
 }
 
-const getTask = (req,res)=>{
-    res.json("Get one Task")
+const getTask = async (req,res)=>{
+    try {
+        const {id:taskID}=req.params
+        const task = await Task.findOne({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})
+    }
 }
 
 const updateTask = (req,res)=>{
