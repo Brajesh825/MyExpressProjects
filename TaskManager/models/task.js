6dd6b5c4535720f15e0b5688f5a55f88b7837f17
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-10 17:12:14 +0530

    Handled error while creating task and added Validators in the task schema

--- TaskManager/models/task.js
+++ TaskManager/models/task.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
+    name:{
+        type:String,
+        required:[true,'must provide name'],
+        trim:true,
+        maxlength:[25,'name can not be more than 25 characters long']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    }
 })
 
 module.exports = mongoose.model('Task',TaskSchema)
