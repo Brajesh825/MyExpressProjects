e20d1051e5da4c5cee0ae5981fd6c72d7c3708df
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-11 15:26:15 +0530

    Update Task Api added

--- TaskManager/controllers/tasks.js
+++ TaskManager/controllers/tasks.js
@@ -31,10 +31,6 @@ const getTask = async (req,res)=>{
     }
 }
 
-const updateTask = (req,res)=>{
-    res.json("Update Task")
-}
-
 const deleteTask = async (req,res)=>{
     try {
         const {id:taskID} = req.params
@@ -47,7 +43,21 @@ const deleteTask = async (req,res)=>{
         res.status(500).json({msg:error})
     }
 }
-
+const updateTask = async (req,res)=>{
+    try {
+        const {id:taskID}=req.params
+        const task = await Task.findOneAndUpdate({_id:taskID},req.body,{
+            new:true,
+            runValidators:true
+        })
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg:error})        
+    }
+}
 module.exports = 
 {   getAllTasks,
     createTask,
