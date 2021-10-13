0b0c2913be735e78f0cf4db8bd6161af14a0b799
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-13 23:12:50 +0530

    Testd the database Connection

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -1,9 +1,14 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req,res)=>{
+    const products = await Product.find({
+        featured:true
+    })
+    res.status(200).json({products,nbHits: products.length})
+}
 
 const getAllProducts = async (req,res)=>{
-    res.status(200).send({msg:"Product Route"})
-}
-const getAllProductsStatic = async (req,res)=>{
-    res.status(200).send({msg:"Product Testing Route"})
+    res.status(200).json({msg:"Product Route"})
 }
 
 module.exports = {getAllProducts,getAllProductsStatic}
