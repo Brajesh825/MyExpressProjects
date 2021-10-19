87335222c11677ea0c37cf0f4bc3776ea1711b64
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-20 00:00:30 +0530

    Store API Project Completed

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req,res)=>{
 
 const getAllProducts = async (req,res)=>{
 
-    const {name,company,featured,sort,fields} =req.query
+    const {name,company,featured,sort,fields,numericFilters} =req.query
     queryObject={}
     // Filter
     if(name)
@@ -22,6 +22,29 @@ const getAllProducts = async (req,res)=>{
     {
         queryObject.featured = featured === 'true' ?true:false
     }
+    //Numeric Filters
+    if(numericFilters){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '<': '$st',
+            '<=': '$ste',
+            '=': '$eq',
+        }
+        const regEx = /\b(<|>|<=|>=|=)\b/g
+        let filters = numericFilters.replace(
+            regEx,
+            (match)=>`-${operatorMap[match]}-`
+        )
+        const options = ['price','rating']
+        filters=filters.split(',').forEach((item)=> {
+            const [field,operator,value]=item.split('-')
+            if(options.includes(field)){
+                queryObject[field]={[operator]:Number(value)}
+            }
+        })
+    } 
+
     // Result
     let result = Product.find(queryObject)
     // Sort
@@ -38,13 +61,13 @@ const getAllProducts = async (req,res)=>{
     }
     // Page
     const page = Number(req.query.page) || 1
-    //limit
+    // limit
     const limit = Number(req.query.limit) || 10
     // Skip
     const skip = (page-1)*limit
     result=result.skip(skip).limit(limit)
 
-    //end products
+    //End products
     products =  await result
     res.status(200).json({products,nbHits: products.length})
 }
