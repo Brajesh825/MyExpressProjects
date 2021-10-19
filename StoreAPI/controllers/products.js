204e7d612e47036737461096a5f6fde72d3a7949
Brajesh825 <brajeshmishra825@gmail.com>
2021-10-19 20:33:53 +0530

    added select

--- StoreAPI/controllers/products.js
+++ StoreAPI/controllers/products.js
@@ -1,16 +1,15 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res)=>{
-    const products = await Product.find({
-        featured:true
-    })
+    const products = await Product.find({})
     res.status(200).json({products,nbHits: products.length})
 }
 
 const getAllProducts = async (req,res)=>{
 
-    const {name,company,featured,sort} =req.query
+    const {name,company,featured,sort,fields} =req.query
     queryObject={}
+    // Filter
     if(name)
     {
         queryObject.name = { $regex :name ,$option: 'i'}
@@ -23,13 +22,21 @@ const getAllProducts = async (req,res)=>{
     {
         queryObject.featured = featured === 'true' ?true:false
     }
-
+    // Result
     let result = Product.find(queryObject)
+    // Sort
     if(sort){
         const sortList = sort.split(',').join(' ')
         result = result.sort(sortList)
     }else{
         result = result.sort('featured')
+    }
+    //Fields
+    if(fields){
+        const fieldList = fields.split(',').join(' ')
+        result = result.select(fieldList)
+    }else{
+        // result = result.select('')
     }
     products =  await result
     res.status(200).json({products,nbHits: products.length})
