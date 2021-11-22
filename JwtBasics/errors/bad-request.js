a1f25fa7e0da75143d73a0528aa8b8ed65b046b8
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-22 13:41:09 +0530

    Completed error

---
+++ JwtBasics/errors/bad-request.js
@@ -0,0 +1,12 @@
+const CustomAPIError = require('./custom-error')
+const {statusCodes} = require('http-status-codes')
+
+class BadRequest extends CustomAPIError {
+    constructor(message) {
+      super(message)
+      this.statusCode = statusCodes.BadRequest
+    }
+}  
+  
+  module.exports = BadRequest
+  
