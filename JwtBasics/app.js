0e3b3a3e87b89ea934d6bf12e63ae6d8ba7118c0
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-22 12:39:19 +0530

    modified JWT

--- JwtBasics/app.js
+++ JwtBasics/app.js
@@ -4,8 +4,6 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 const mainrouter = require('./routes/main')
-
-
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
--- JwtBasics/controllers/main.js
+++ JwtBasics/controllers/main.js
@@ -15,13 +15,31 @@ const login = async(req,res)=>{
     if(!username || !password){
         throw new CustomAPIError('Please Provide email and password',400)
     }
-    // just for demo
+    // try to keep payload small,better experience for user
+    // just for demo, in production use long, complex and unguessable string value !!!!!!
+    // just for demo, Id is provided by the database
     const id = new Date().getDate()
     const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
     res.status(200).json({msg:`user created`,token: token})
 }
 
 const dashboard = async (req,res) =>{
+    const authHeader = req.headers.authorization;
+    // console.log(authHeader)
+
+    if(!authHeader || !authHeader.startsWith('Bearer '))
+    {
+        throw new CustomAPIError('No Token Provided',401)
+    }
+    const token = authHeader.split(" ")[1]
+
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log(decoded);
+    }catch(error){
+       throw new CustomAPIError('Not authorized to access this route',401)
+    }    
+
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg:`Hello, Brajesh`,secret:`Your lucky number is ${luckyNumber}`})
 }
