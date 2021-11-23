a7dc283ca8bc7409fc27a3c83b256f9e86a63e25
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-24 00:05:43 +0530

    Added Encryption to password

--- JobsAPI/models/User.js
+++ JobsAPI/models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
     },
     email:{
         type:String,
-        required: [true,"Please provide name"],
+        required: [true,"Please provide email"],
         match: [
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             "Please provide valid email"
@@ -18,9 +18,7 @@ const UserSchema = new mongoose.Schema({
     },
     password:{
         type:String,
-        required:[true,"Please provide Password"],
-        minlength: 3,
-        maxlength: 50
+        required:[true,"Please provide Password"]
     },
 })
 
