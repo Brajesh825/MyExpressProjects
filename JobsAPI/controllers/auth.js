82a4182921329b0eef3d04c13ae870b5cb46560e
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-24 15:35:27 +0530

    Created Login Route

--- JobsAPI/controllers/auth.js
+++ JobsAPI/controllers/auth.js
@@ -1,18 +1,30 @@
 
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
+const {BadRequestError,UnauthenticatedError} = require('../errors')
 
 
 const register =async (req,res) =>{
     const user = await User.create({...req.body})
     const token = user.createJWT()
-    console.log(token)
     res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
 }
 
 const login = async (req,res) =>{
-    res.send('login user')
+    const {email,password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    // Compare Password
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user:{name:user.name},token})
+
 }
 
 module.exports ={
