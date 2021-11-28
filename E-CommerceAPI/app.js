1870eaafc8999034045da5e846073d86dccb0910
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-28 15:43:57 +0530

    Created Login and Logout route

--- E-CommerceAPI/app.js
+++ E-CommerceAPI/app.js
@@ -8,6 +8,7 @@ const app = express();
 
 // rest of the packages
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
 
 // Database
 const connectDB = require('./db/connect')
@@ -21,6 +22,13 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+
+// Home route
+app.get('/api/v1',(req,res)=>{
+    console.log(req.signedCookies);
+    res.send('e-commerce api')
+})
 
 // routes
 app.use('/api/v1/auth/',authRouter)
