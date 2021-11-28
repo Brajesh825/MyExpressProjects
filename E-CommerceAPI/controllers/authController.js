76a2a5a9255674af184d9b8f8543e94828b0392d
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-28 13:21:29 +0530

    Cookies Added

--- E-CommerceAPI/controllers/authController.js
+++ E-CommerceAPI/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const {createJWT} = require('../utils')
 
 const register = async (req,res) =>{
     const {email,name,password} = req.body
@@ -15,7 +16,16 @@ const register = async (req,res) =>{
     const role = isFirstAccount ? 'admin' :'user'
 
     const user = await User.create({email,name,password,role})
-    res.status(StatusCodes.CREATED).json({user})
+    const tokenUser = {name:user.name,userId:user._id,role: user.role}
+    const token = createJWT({payload : tokenUser})
+
+    const oneDay = 1000 * 60 * 60 * 24
+
+    res.cookie('token',token,{
+        httpOnly:true,
+        expires:new Date(Date.now() + oneDay)
+    })
+    res.status(StatusCodes.CREATED).json({user:tokenUser, token})
 }
 
 const login = async (req,res) =>{
