7262730f83d12e6df3e83edd92a82bbe4e235a3b
Brajesh825 <brajeshmishra825@gmail.com>
2021-11-30 01:12:33 +0530

    Completed User route with all permissions

--- E-CommerceAPI/models/User.js
+++ E-CommerceAPI/models/User.js
@@ -31,6 +31,8 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.pre('save',async function(){
+    // Checking whether password was modified or not
+    if(!this.isModified('password')) return
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
