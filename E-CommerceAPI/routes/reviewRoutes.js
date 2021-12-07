5219730ceeb2032c17316acbecdbcaac3929ef6d
Brajesh825 <brajeshmishra825@gmail.com>
2021-12-07 22:51:48 +0530

    Completed product schema and working on order schema

--- E-CommerceAPI/routes/reviewRoutes.js
+++ E-CommerceAPI/routes/reviewRoutes.js
@@ -1,26 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
-const {authenticateUser} 
-    = require('../middleware/authentication')
+const { authenticateUser } = require('../middleware/authentication')
 
 const {
-    createReview,
-    getAllReviews,
-    getSingleReview,
-    updateReview,
-    deleteReview
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+  getSingleProductReview,
 } = require('../controllers/reviewController')
 
-router
-    .route('/')
-    .post(authenticateUser,createReview)
-    .get(getAllReviews)
+router.route('/').post(authenticateUser, createReview).get(getAllReviews)
 
 router
-    .route('/:id')
-    .get(getSingleReview)
-    .patch(authenticateUser,updateReview)
-    .delete(authenticateUser,deleteReview)
+  .route('/:id')
+  .get(getSingleReview)
+  .patch(authenticateUser, updateReview)
+  .delete(authenticateUser, deleteReview)
 
 module.exports = router
